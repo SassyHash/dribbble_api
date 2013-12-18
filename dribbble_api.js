397b22376451fded18209a2bf27e8d71fdf31be5
Sassy <b@sassyha.sh>
2013-12-18 13:54:04 -0800

    fixed image tags and styled some

--- dribbble_api.js
+++ dribbble_api.js
@@ -1,42 +1,76 @@
-var shotsArray = new Array();
+var option = option || "/popular"
 
-var renderShots = function(){
-  console.log(shotsArray.shots);
-};
-
-var ajaxRequest = function(option){
-  option = option || "/popular"
-  console.log(option);
+var ajaxRequest = function(page){
+  console.log("called");
+  page = page || 1
   $.ajax({
-        url           : "http://api.dribbble.com/shots" + option + "?per_page=20",
-        dataType      : "jsonp",
+        url : "http://api.dribbble.com/shots" + option + "?per_page=18&page=" + page,
+        dataType : "jsonp",
         jsonpCallback : "dealJSONP"
       });
 }
 
 var dealJSONP = function(data){
-  shots = data;
+  var shotsArray = new Array();
+  shotsArray = data.shots;
+  console.log(data.page);
+  console.log(data.per_page);
+
   if (shotsArray.length > 0) {
-    renderShots();
+    paginate(data.total, data.per_page);
+    clearPage();
+    _.each(shotsArray, renderShot);
   } else {
     $(".row").append("<h1> Oops. Something's wrong. There are no shots.</h1>");
   }
 };
 
-  ajaxRequest();
+var clearPage = function(){
+  $(".shots").empty();
+};
+
+
+var renderShot = function(shot){
+  var shotElement = $('<img>'); 
+  console.log(shot.id);
+  shotElement.attr('class', 'shot');
+  shotElement.attr('src', shot.image_url);
+
+  $('.shots').append(shotElement);
+
+  //$(".shots").append(
+  //  shot.image_400_url
+  //   );
+};
+
+
+var paginate = function(total_items, itemspp){
+  $(".pages").pagination({
+      items: total_items,
+      itemsOnPage: itemspp,
+      displayedPages: 3
+    });
+}
+
+
+ajaxRequest();
 
 $(document).ready(function(){
 
   $("#everyone").click(function(){
-    console.log("clicked");
-    ajaxRequest("/everyone");
+    option = "/everyone";
+    ajaxRequest();
   })
 
   $("#popular").click(function(){
-    ajaxRequest("/popular");
-  })
-  $(".pages").click(function(){
+    option = "/popular";
+    ajaxRequest();
 
+  })
+  $(".page-link").click(function(){
+    var page = this.text();
+    ajaxRequest(page);
+    console.log(page);
   })  
 })
 
