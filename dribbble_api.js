b894b26d82fc23340d802cdbc119850e5d6d854f
Sassy <b@sassyha.sh>
2013-12-17 20:05:01 -0800

    broke jsonp ugh

--- dribbble_api.js
+++ dribbble_api.js
@@ -1,22 +1,43 @@
+var shotsArray = new Array();
+
+var renderShots = function(){
+  console.log(shotsArray.shots);
+};
+
 var ajaxRequest = function(option){
   option = option || "/popular"
   console.log(option);
   $.ajax({
-        url           : 'http://api.dribbble.com/shots' + option + '?per_page=40?page=5',
-        dataType      : 'jsonp',
-        jsonpCallback : 'dealJSONP'
+        url           : "http://api.dribbble.com/shots" + option + "?per_page=20",
+        dataType      : "jsonp",
+        jsonpCallback : "dealJSONP"
       });
 }
 
-
 var dealJSONP = function(data){
-  console.log(data);
-  $('.row').html("<span><h1> data here </h1></span>");
+  shots = data;
+  if (shotsArray.length > 0) {
+    renderShots();
+  } else {
+    $(".row").append("<h1> Oops. Something's wrong. There are no shots.</h1>");
+  }
 };
 
+  ajaxRequest();
+
+$(document).ready(function(){
+
+  $("#everyone").click(function(){
+    console.log("clicked");
+    ajaxRequest("/everyone");
+  })
+
+  $("#popular").click(function(){
+    ajaxRequest("/popular");
+  })
+  $(".pages").click(function(){
+
+  })  
+})
 
 
-    
-
-
-ajaxRequest("/everyone");
