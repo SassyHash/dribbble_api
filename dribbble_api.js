8e81f0733b74c6edda2c7cf2ac5e5031d84bbc51
Sassy <b@sassyha.sh>
2013-12-18 16:19:41 -0800

    jquery pages fixed

--- dribbble_api.js
+++ dribbble_api.js
@@ -1,10 +1,11 @@
-var option = option || "/popular"
-
+var option = option || "/popular";
+var itemsPp = itemsPp || 18;
 var ajaxRequest = function(page){
-  console.log("called");
+  console.log(itemsPp);
+  console.log(page);
   page = page || 1
   $.ajax({
-        url : "http://api.dribbble.com/shots" + option + "?per_page=18&page=" + page,
+        url : "http://api.dribbble.com/shots" + option + "?per_page=" + itemsPp +"&page=" + page,
         dataType : "jsonp",
         jsonpCallback : "dealJSONP"
       });
@@ -13,15 +14,14 @@ var ajaxRequest = function(page){
 var dealJSONP = function(data){
   var shotsArray = new Array();
   shotsArray = data.shots;
-  console.log(data.page);
-  console.log(data.per_page);
 
   if (shotsArray.length > 0) {
-    paginate(data.total, data.per_page);
     clearPage();
+    paginate(data.total, data.page);  
     _.each(shotsArray, renderShot);
   } else {
-    $(".row").append("<h1> Oops. Something's wrong. There are no shots.</h1>");
+    clearPage();
+    $(".shots").append("<h1> Oops. Something's wrong. There are no shots.</h1>");
   }
 };
 
@@ -35,43 +35,54 @@ var renderShot = function(shot, index){
   shotElement.attr("class", "shot-"+index);
   shotElement.attr("src", shot.image_url);
   $(".shots").append(shotElement);
-  $(".shot-"+index).wrap(function(){
-    return "<a href="+ shot.url+"></a>";
-  });
-  //$(".shots").append(
-  //  shot.image_400_url
-  //   );
+  $(".shot-"+index).wrap("<a href="+ shot.url+"></a>");
 };
 
-
-var paginate = function(total_items, itemspp){
+var paginate = function(total_items, page){
   $(".pages").pagination({
       items: total_items,
-      itemsOnPage: itemspp,
-      displayedPages: 3
+      itemsOnPage: itemsPp,
+      currentPage: page,
+      displayedPages: 3,
+      onPageClick: pageFlip
     });
 }
 
+var pageFlip = function(page){
+  ajaxRequest(page);
+}
 
 ajaxRequest();
 
+
 $(document).ready(function(){
+  $("#min").hide();
 
   $("#everyone").click(function(){
     option = "/everyone";
     ajaxRequest();
-  })
+  });
 
   $("#popular").click(function(){
     option = "/popular";
     ajaxRequest();
+  });
 
+  $("#max").click(function(){
+    itemsPp = 30;
+    ajaxRequest();
+    $("#max").hide();
+    $("#min").show();
+  });
+
+  $("#min").click(function(){
+    itemsPp = 18;
+    ajaxRequest();
+    $("#min").hide();
+    $("#max").show();
   })
-  $(".page-link").click(function(){
-    var page = this.text();
-    ajaxRequest(page);
-    console.log(page);
-  })  
+  
+ 
 })
 
 
